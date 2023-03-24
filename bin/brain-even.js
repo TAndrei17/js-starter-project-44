29e9be69b08c5f2bc4bc329ee48f8c3aa76dea83
Andrei Trunkin <inboxavt@gmail.com>
2023-03-24 23:37:25 +0100

    Step 5. Game "Parity check". DRAFT

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,8 +1,10 @@
 import readlineSync from 'readline-sync';
 
-const game = () => {
-  const nums = {
-    current: '',
+const game = (name) => {
+  const state = {
+    currentNum: null,
+    even: null,
+    answer: '',
     right: 0,
     wrong: 0,
   };
@@ -11,47 +13,54 @@ const game = () => {
     rule: 'Answer "yes" if the number is even, otherwise answer "no".',
     yesWrong: "'yes' is wrong answer ;(. Correct answer was 'no'.",
     noWrong: "'no' is wrong answer ;(. Correct answer was 'yes'.",
-    incorrect: "Let's try again",
+    incorrect: `Let's try again, ${name}!`,
     correct: 'Correct!',
-    congrats: 'Congragulations',
+    congrats: `Congragulations, ${name}!`,
+  };
+
+  const getNumber = () => {
+    const maxNumber = 250;
+    const number = Math.floor(Math.random() * maxNumber);
+    state.currentNum = number;
+    state.even = (number % 2 === 0);
+    const askNumber = () => `Question: ${state.currentNum}`;
+    console.log(askNumber());
+    const playerAnswer = readlineSync.question('Your answer: ');
+    state.answer = playerAnswer;
+  };
+
+  const render = (answer, even) => {
+    let result = '';
+    if ((answer === 'yes' && even) || (answer === 'no' && !even)) {
+      result = dialogs.correct;
+    } else if (answer === 'yes' && !even) {
+      result = dialogs.yesWrong;
+    } else if (answer === 'no' && even) {
+      result = dialogs.noWrong;
+    } else {
+      result = dialogs.incorrect;
+    }
+    if (result !== dialogs.incorrect) {
+      console.log(result);
+    }
+    state.right = (result === dialogs.correct) ? state.right += 1 : state.right;
+    state.wrong = (result !== dialogs.correct) ? state.wrong += 1 : state.wrong;
   };
 
   console.log(dialogs.rule);
-
-  const getNumber = () => {
-    const maxNumber = 100;
-    const number = Math.floor(Math.random() * maxNumber);
-    nums.current = number;
-    return number;
-  };
-
-  const checkNumber = () => {
+  const brainEven = () => {
     getNumber();
-    const askNumber = () => `Question: ${nums.current}`;
-    console.log(askNumber());
-    const playerAnswer = readlineSync.question('Your answer: ');
-    return playerAnswer;
-  };
+    render(state.answer, state.even);
 
-  const render = (answer, even = true) => {
-    if (answer === 'yes') {
-      const result = (even) ? console.log(dialogs.correct) : console.log(dialogs.yesWrong);
-      return (result === true) ? checkNumber() : dialogs.incorrect;
-    }
-    if (answer === 'no') {
-      const result = (even) ? console.log(dialogs.correct) : console.log(dialogs.noWrong);
-      return (result === true) ? checkNumber() : dialogs.incorrect;
+    if (state.right < 3 && state.wrong === 0) {
+      return brainEven();
+    } if (state.right === 3 && state.wrong === 0) {
+      console.log(dialogs.congrats);
+    } else if (state.wrong > 0) {
+      console.log(dialogs.incorrect);
     }
   };
-
-  /* const brainEven = (name) => {
-    const answer = checkNumber();
-    const isEven = nums.current % 2;
-    if ((answer === 'yes' && isEven) || (answer === 'no' && !isEven)) {
-      nums.right += 1;
-    } else {
-      nums.wrong += 1;
-    }
-    render(answer, isEven);
-  }; */
+  return brainEven();
 };
+
+export default game;
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import greeting from '../cli.js';
+import game from './brain-even.js';
 
 console.log('Welcome to the Brain Games!');
 
-greeting();
+const name = greeting();
+game(name);
