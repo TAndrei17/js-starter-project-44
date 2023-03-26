5012dc646e9a4b9e05bdd29902baf0ad578b3be6
Andrei Trunkin <inboxavt@gmail.com>
2023-03-26 22:13:55 +0200

    Step 5. Update brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
 import greeting from '../cli.js';
-import game from './brain-even.js';
 
 console.log('Welcome to the Brain Games!');
-
-const name = greeting();
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-game(name);
+greeting();
