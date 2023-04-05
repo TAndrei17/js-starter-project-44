02f2f7df81f41e234118831489a890d06e1a09b9
Andrei Trunkin <inboxavt@gmail.com>
2023-04-05 13:19:54 +0200

    Update cli.js

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const greeting = () => {
-  const name = readlineSync.question('Welcome to the Brain Games!\nMay i have your name? ');
+  const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
 };
